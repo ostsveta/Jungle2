f9e0bb33923fa27674e566766ac3384d70bbf6e7
Светлана <sv.agfnva@gmail.com>
2024-10-29 21:44:28 +0400

    Create method "main"

--- src/main/java/ru/ostsveta/forestsimulator/_main/_Main.java
+++ src/main/java/ru/ostsveta/forestsimulator/_main/_Main.java
@@ -1,4 +1,13 @@
 package ru.ostsveta.forestsimulator._main;
 
+import ru.ostsveta.forestsimulator.model.Fox;
+import ru.ostsveta.forestsimulator.util.EventCreate;
+
 public class _Main {
+
+    public static void main(String[] args) {
+        Fox fox = new Fox;
+        EventCreate eventCreate = new EventCreate();
+        eventCreate.activateSimulator(fox);
+    }
 }
